2e8f3493d7e46ba5381f3d645b9bd5bc4dd70888
Vivasaayi <rajan.india@yahoo.co.in>
2019-03-08 00:36:29 -0500

    Test

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,9 +1,124 @@
 const assert = require('assert');
 
 describe('webdriver.io page', () => {
-    it('should have the right title', () => {
-        browser.url('https://webdriver.io');
-        const title = browser.getTitle();
-        assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
-    });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
+  it('should have the right title', () => {
+    browser.url('http://poriyiyal.azurewebsites.net/');
+    const title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    browser.pause(3000)
+  });
 });
